f2977ce007a6acd52b4b05c2ad0c5bad12f396cd
IndiraPe <forever_tk_16@hotmail.com>
2023-11-05 13:40:28 -0500

    ADD: page Home and componentes

---
+++ src/app/gifs/gifs.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    SearchBoxComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HomeComponent,
+  ]
+})
+export class GifsModule { }
